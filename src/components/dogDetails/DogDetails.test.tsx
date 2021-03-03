b8c7c38cab37986652d5a7a86e4fa1aa34a08fec
Thiago <thiagocampos@ciandt.com>
2021-03-03 10:47:44 -0300

    [BEESPI-5047] - made tests of state scold count

--- src/components/dogDetails/DogDetails.test.tsx
+++ src/components/dogDetails/DogDetails.test.tsx
@@ -3,9 +3,26 @@ import DogDetails from './DogDetails';
 import DogDetailsView from './DogDetailsView';
 
 describe('DogDetails', () => {
-  const mockFn = jest.fn();
-  const wrapper = shallow(<DogDetails name={'teste'} urlImage={'teste'} onBark={mockFn} />);
   it('renders component with his props', () => {
-    expect(wrapper.type()).toBe(DogDetailsView);
+    const mockFn = jest.fn();
+    const wrapper = shallow(<DogDetails name={'teste'} urlImage={'teste'} onBark={mockFn} />);
+    expect(
+      wrapper.matchesElement(
+        <DogDetailsView
+          urlImage={'teste'}
+          name={'teste'}
+          onBark={mockFn}
+          scoldCount={0}
+          onScold={mockFn}
+        />
+      )
+    );
+  });
+
+  it('should add + 1 to scold count when call onScold function', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(<DogDetails name={'teste'} urlImage={'teste'} onBark={mockFn} />);
+    wrapper.invoke('onScold')();
+    expect(wrapper.prop('scoldCount')).toBe(1);
   });
 });

d15f8ff549035015b5451661628cb907cbd1d76f
Thiago <thiagocampos@ciandt.com>
2021-03-09 13:54:15 -0300

    [BEESPI-5053] - style dogdetails

--- src/components/dogDetails/DogDetailsView.styles.ts
+++ src/components/dogDetails/DogDetailsView.styles.ts
@@ -2,19 +2,20 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const dogDetailsStyle = makeStyles({
   containerImage: {
-    padding: '0px 0px 20px 0px',
+    padding: '20px 0px 20px 0px',
     borderRadius: '5px',
     background: '#FFDEAF',
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'column',
     '& img': {
+      height: 130,
       borderRadius: '5px 5px 0 0',
     },
     '& span': {
       opacity: '0.7',
       marginTop: 10,
-      fontSize: 30,
+      fontSize: 23,
       fontWeight: '600',
     },
     '& button': {
